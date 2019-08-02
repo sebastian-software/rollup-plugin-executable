abcb9b1702072f772cee6752b5fcd44023589fe2
Sebastian Fastner <s.fastner@sebastian-software.de>
2019-08-02 12:31:19 +0200

    chore(ts): Upgrade to TS

---
+++ src/index.test.ts
@@ -0,0 +1,31 @@
+import fs from "fs"
+import { promisify } from "util"
+
+import executablePlugin from "."
+
+const stat = promisify(fs.stat)
+const writeFile = promisify(fs.writeFile)
+const removeFile = promisify(fs.unlink)
+
+test("Makes executable", async () => {
+  await writeFile("testfile", "testdata", {
+    mode: 0o644
+  })
+
+  const plugin = executablePlugin()
+  plugin.generateBundle({
+    file: "testfile"
+  })
+  plugin.writeBundle()
+
+  const { mode } = await stat("testfile")
+
+  await removeFile("testfile")
+
+  if (process.platform === "win32") {
+    // on windows changing file mode has no influence to file mode
+    expect(true).toBe(true)
+  } else {
+    expect(mode).toBe(0o100755)
+  }
+})
